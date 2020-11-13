e380a4baedf8190c55653cbf1b4402b93a0ec913
Joey3001 <josephnero111@gmail.com>
2020-11-13 11:53:26 -0800

    Add marsYears prototype. Achieve good fail.

--- src/js/age.js
+++ src/js/age.js
@@ -3,12 +3,14 @@ export default class Age {
     this.startingAge = start; 
     this.mercuryAge = 0; 
     this.venusAge = 0;
+    this.marsAge = 0; 
   }
-
   mercuryYears() {
     this.mercuryAge = this.startingAge/.24; 
   }
   venusYears() {
     this.venusAge = this.startingAge/.64;
+  }
+  marsYears() {
   }
 }
