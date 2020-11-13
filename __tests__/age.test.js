c222353c645466fea7363b16e49e9539fd83fda8
Joey3001 <josephnero111@gmail.com>
2020-11-13 14:04:54 -0800

    Update test spec for remainingYears in tests file

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -7,34 +7,36 @@ describe('Age', () => {
     newAge = new Age(20);
   });
 
-  test('should correctly create an instance of the Age object', () => {
+  test('Should correctly create an instance of the Age object', () => {
     expect(typeof(newAge)).toEqual("object");
   });
 
-  test('should correctly create an instance of the Age object with input provided to determine the starting age', () => {
+  test('Should correctly create an instance of the Age object with input provided to determine the starting age', () => {
     expect(newAge.startingAge).toEqual(20);
   });
 
-  test(' Should correctly return a users age in mercury years (A mercury year is .24 earth years)', () => {
+  test('Should correctly return a users age in mercury years (A mercury year is .24 earth years)', () => {
     newAge.mercuryYears(); 
     expect(newAge.mercuryAge).toEqual(20/.24);
   });
 
-  test(' Should correctly return a users age in venus years (A venus year is .64 earth years)', () => {
+  test('Should correctly return a users age in venus years (A venus year is .64 earth years)', () => {
     newAge.venusYears(); 
     expect(newAge.venusAge).toEqual(20/.64);
   });
 
-  test(' Should correctly return a users age in mars years (A mars year is 1.88 earth years)', () => {
+  test('Should correctly return a users age in mars years (A mars year is 1.88 earth years)', () => {
     newAge.marsYears(); 
     expect(newAge.marsAge).toEqual(20/1.88);
   });
 
-  test(' Should correctly return a users age in jupier years (A jupiter year is 1.88 earth years)', () => {
+  test('Should correctly return a users age in jupier years (A jupiter year is 11.86 earth years)', () => {
     newAge.jupiterYears(); 
     expect(newAge.jupiterAge).toEqual(20/11.86);
   });
 
-  test(' Should correctly return a users remaining years as calculated with average life expectancy', () => {
+  test('Should correctly return a users remaining years as calculated with average life expectancy in the US', () => {
+    newAge.remainingYears();
+    expect(newAge.remainingYears).toEqual(78.54-20);
   });
 })
