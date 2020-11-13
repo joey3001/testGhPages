6043fdc86864bcc575d7f7c1d8eee7a30d968996
Joey3001 <josephnero111@gmail.com>
2020-11-13 11:39:25 -0800

    Add venusYears prototype to Age class. get good fail.

--- src/js/age.js
+++ src/js/age.js
@@ -2,9 +2,12 @@ export default class Age {
   constructor(start) {
     this.startingAge = start; 
     this.mercuryAge = 0; 
+    this.venusAge = 0;
   }
 
   mercuryYears() {
     this.mercuryAge = this.startingAge/.24; 
+  }
+  venusYears() {
   }
 }
