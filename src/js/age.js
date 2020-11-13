80e780c607c9cd3371366de946921e8cdc31c7c6
Joey3001 <josephnero111@gmail.com>
2020-11-13 11:22:06 -0800

    Add feature to mercuryYears. Achieve passing test.

--- src/js/age.js
+++ src/js/age.js
@@ -1,8 +1,10 @@
 export default class Age {
   constructor(start) {
     this.startingAge = start; 
+    this.mercuryAge = 0; 
   }
 
   mercuryYears() {
+    this.mercuryAge = this.startingAge/.24; 
   }
 }
