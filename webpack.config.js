e0f59fbd31d4ab703b4cfd6f000d572f64c23fe7
Joey3001 <josephnero111@gmail.com>
2020-11-14 23:30:27 -0800

    Add more custom styling

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'galactic-age-calculator',
+      title: 'Shape Tracker',
       template: './src/index.html',
       inject: 'body'
     })
@@ -33,7 +33,26 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "eslint-loader"
-      }
+      },
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      
+      {
+        test:/\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
     ]
   }
 };
