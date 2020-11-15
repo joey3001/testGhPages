482695de75481371e6f6db344c19a5ea00d13f25
Joey3001 <josephnero111@gmail.com>
2020-11-15 13:50:15 -0800

    Add feature to lifeExpectancyBoolean. Pass test

--- src/js/age.js
+++ src/js/age.js
@@ -38,6 +38,12 @@ export default class Age {
   }
 
   lifeExpectancyBoolean() {
+    if (this.startingAge > 78.54) {
+      return false;
+    }
+    else {
+      return true;
+    }
   }
 
   remainingMercuryYears() {
