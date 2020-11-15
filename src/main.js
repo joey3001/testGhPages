2586c834731a19773df4925016fb5bef13064fc1
Joey3001 <josephnero111@gmail.com>
2020-11-15 00:40:14 -0800

    Start building UI

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Age from './js/age.js';
+
+$('#calculator').submit(function(event) {
+  event.preventDefault();
+  let newAge = new Age(parseInt($('number').val()));
+  newAge.mercuryYears();
+  newAge.venusYears();
+  newAge.marsYears();
+  newAge.jupiterYears();
+  newAge.remainingYears();
+  newAge.remainingMercuryYears();
+  newAge.remainingVenusYears();
+  newAge.remainingMarsYears();
+  newAge.remainingJupiterYears(); 
+  console.log(newAge);
+});
